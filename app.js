486aa6dccc0b2a7ffa61798dd30c8d0bbcdd8240
Adam East <adam.eastvold@gmail.com>
2016-08-01 08:11:49 -0500

    cleaned up - hard mode

--- app.js
+++ app.js
@@ -1,68 +1,57 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  var monthlyAmount = 0;
-  var totalMonthly = 0;
+    var monthlyAmount = 0;
+    var totalMonthly = 0;
 
-  $('#employeeinfo').on('submit', function(){
-      event.preventDefault();
+    $('#employeeinfo').on('submit', function() {
+        event.preventDefault();
 
-      var employee = {};
-      // var monthlyAmount = 0;
-      // var totalMonthly = 0;
-      var fields = $('#employeeinfo').serializeArray(); //DO NOT TOUCH
+        var employee = {};
 
-      fields.forEach(function(element, index, array){
-        employee[element.name] = element.value;         //CHECK THIS OUT AGAIN, UNDERSTAND THIS MORE CLEARLY
-      });
+        var fields = $('#employeeinfo').serializeArray(); //DO NOT TOUCH
 
-      console.log(employee);
+        fields.forEach(function(element, index, array) {
+            employee[element.name] = element.value;
+        });
+
+        // console.log(employee);
+
+        $('#employeeinfo').find('input[type=text]').val('');
 
 
+        appendDom(employee);
 
-      $('#employeeinfo').find('input[type=text]').val('');
 
-      appendDom(employee);
+        function appendDom(empInfo) {
+            $('#container').append('<div class="person"></div>');
+            var $el = $('#container').children().last();
 
-      function appendDom(empInfo) {
-        $('#container').append('<div class="person"></div>');
-        var $el = $('#container').children().last();
-        // var monthlyAmount = 0;
-        // var totalMonthly = 0;
 
-        employee.employeeSalary = parseInt(employee.employeeSalary);  //change type to number
-        console.log(typeof employee.employeeSalary);
+            employee.employeeSalary = parseInt(employee.employeeSalary); //change type to number
+            console.log(typeof employee.employeeSalary);
 
 
 
-        $el.append('<p class="information">' + empInfo.employeeFirstName + ' ' + empInfo.employeeLastName + ' | ' + empInfo.employeeID + ' | ' + empInfo.employeeJobTitle + ' | ' + empInfo.employeeSalary + ' | <button class="delete">Delete</button></p>');
+            $el.append('<p class="information">' + empInfo.employeeFirstName +
+                ' ' + empInfo.employeeLastName + ' | ' + empInfo.employeeID +
+                ' | ' + empInfo.employeeJobTitle + ' | ' + empInfo.employeeSalary +
+                ' | <button class="delete">Delete</button></p>');
 
 
+            monthlyAmount = parseInt(employee.employeeSalary) / 12;
+            totalMonthly = totalMonthly + monthlyAmount;
 
 
-        monthlyAmount = parseInt(employee.employeeSalary) / 12;
-        totalMonthly = totalMonthly + monthlyAmount;
+            $('.monthlyRate').empty();
+            $('#counter').append('<p class="monthlyRate">$' + totalMonthly + ' per Month</p>');
 
+        }
 
-        // monthlyAmount = (employee.employeeSalary) / 12;
+        $('.information').click(function() {
+            console.log('what');
+            $(this).remove('.information');
+        });
 
-        // $('.monthlyRate').each(function(){
-        //   employee.employeeSalary = parseInt(employee.employeeSalary);     <---- original
-        //   totalMonthly = totalMonthly + monthlyAmount;
-        //   console.log(typeof totalMonthly);     //Close to success?????
-        // });
-
-        $('.monthlyRate').empty();
-        $('#counter').append('<p class="monthlyRate">$' + totalMonthly + ' per Month</p>');
-
-
-      }
-
-
-      $('.information').click(function(){
-        console.log('what');
-        $(this).remove('.information');
-      });
-
-  });
+    });
 
 });
