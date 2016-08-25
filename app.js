e286755455167b9278a2ae614cec676be9a30710
Adam East <adam.eastvold@gmail.com>
2016-08-25 14:56:13 -0500

    angular commit

--- app.js
+++ app.js
@@ -1,57 +1,59 @@
-$(document).ready(function() {
+var myApp = angular.module("myApp", []);
 
-    var monthlyAmount = 0;
-    var totalMonthly = 0;
-
-    $('#employeeinfo').on('submit', function() {
-        event.preventDefault();
-
-        var employee = {};
-
-        var fields = $('#employeeinfo').serializeArray(); //DO NOT TOUCH
-
-        fields.forEach(function(element, index, array) {
-            employee[element.name] = element.value;
-        });
-
-        // console.log(employee);
-
-        $('#employeeinfo').find('input[type=text]').val('');
-
-
-        appendDom(employee);
-
-
-        function appendDom(empInfo) {
-            $('#container').append('<div class="person"></div>');
-            var $el = $('#container').children().last();
-
-
-            employee.employeeSalary = parseInt(employee.employeeSalary); //change type to number
-            // console.log(typeof employee.employeeSalary);
-
-
-
-            $el.append('<p class="information">' + empInfo.employeeFirstName +
-                ' ' + empInfo.employeeLastName + ' | ' + empInfo.employeeID +
-                ' | ' + empInfo.employeeJobTitle + ' | ' + empInfo.employeeSalary +
-                ' | <button class="delete">Delete</button></p>');
-
-
-            monthlyAmount = parseInt(employee.employeeSalary) / 12;
-            totalMonthly = totalMonthly + monthlyAmount;
-
-
-            $('.monthlyRate').empty();
-            $('#counter').append('<p class="monthlyRate">$' + totalMonthly + ' per Month</p>');
-
-        }
-
-        $('.information').click(function() {
-            // console.log('what');
-            $(this).remove('.information');
-        });
-
-    });
-
-});
+// $(document).ready(function() {
+//
+//     var monthlyAmount = 0;
+//     var totalMonthly = 0;
+//
+//     $('#employeeinfo').on('submit', function() {
+//         event.preventDefault();
+//
+//         var employee = {};
+//
+//         var fields = $('#employeeinfo').serializeArray(); //DO NOT TOUCH
+//
+//         fields.forEach(function(element, index, array) {
+//             employee[element.name] = element.value;
+//         });
+//
+//         // console.log(employee);
+//
+//         $('#employeeinfo').find('input[type=text]').val('');
+//
+//
+//         appendDom(employee);
+//
+//
+//         function appendDom(empInfo) {
+//             $('#container').append('<div class="person"></div>');
+//             var $el = $('#container').children().last();
+//
+//
+//             employee.employeeSalary = parseInt(employee.employeeSalary); //change type to number
+//             // console.log(typeof employee.employeeSalary);
+//             $('.employeeSalary').data()
+//
+//
+//
+//             $el.append('<p class="information">' + empInfo.employeeFirstName +
+//                 ' ' + empInfo.employeeLastName + ' | ' + empInfo.employeeID +
+//                 ' | ' + empInfo.employeeJobTitle + ' | ' + empInfo.employeeSalary +
+//                 ' | <button class="delete">Delete</button></p>');
+//
+//
+//             monthlyAmount = parseInt(employee.employeeSalary) / 12;
+//             totalMonthly = totalMonthly + monthlyAmount;
+//
+//
+//             $('.monthlyRate').empty();
+//             $('#counter').append('<p class="monthlyRate">$' + totalMonthly + ' per Month</p>');
+//
+//         }
+//
+//         $('.information').click(function() {
+//             $(this).remove('.information');
+//         });
+//
+//     });
+//
+// });
---
+++ controllers/taskController.js
@@ -0,0 +1,77 @@
+myApp.controller("taskController", ["$scope", function($scope) {
+    console.log("task Controller is up and running");
+
+    $scope.employeeArray = [];
+    $scope.employeeInfo = {};
+    console.log($scope.employeeInfo);
+
+
+    var monthlyAmount = 0;
+    $scope.totalMonthly = 0;
+
+    $scope.submitEmployee = function() {
+
+        $scope.employeeInfo.salary = $scope.employeeInfo.salary * 1;
+
+        monthlyAmount = $scope.employeeInfo.salary / 12;
+        $scope.totalMonthly = $scope.totalMonthly + monthlyAmount;
+
+        // $scope.employeeInfo.monthlySalary = totalMonthly;
+
+
+
+        $scope.employeeArray.push($scope.employeeInfo);
+        $scope.employeeInfo = {};
+
+        console.log($scope.employeeArray);
+
+    }
+
+    $scope.deleteEmployee = function(id) {
+        if (confirm("Delete this employee?")) {
+            console.log('delete employee id ', id);
+            // console.log($scope.employeeArray);
+            $scope.employeeArray.forEach(function(employee, i) {
+                console.log(employee);
+                if (employee.ID == id) {
+
+                    monthlyAmount = $scope.employeeInfo.salary / 12;
+                    $scope.totalMonthly = $scope.totalMonthly - monthlyAmount;
+                    console.log($scope.totalMonthly);
+
+
+
+
+                    $scope.tempArray = $scope.employeeArray;
+
+  console.log('this is what needs to get sliced:', $scope.tempArray[i]);
+  $scope.tempArray.slice(i, 1);
+  console.log('this is what the slice slices:', $scope.tempArray.slice(i, 0));
+  $scope.employeeArray = $scope.tempArray;
+  console.log('this is after slice:', $scope.employeeArray);
+                    //create temporary array, get rid of object, push new array into old array, which is now blank
+                    //or
+                    //TRY SPLICE
+                };
+            });
+        };
+
+    };
+
+
+    //     monthlyAmount = parseInt(employee.employeeSalary) / 12;
+    //     totalMonthly = totalMonthly + monthlyAmount;
+    //
+    //
+    //     $('.monthlyRate').empty();
+    //     $('#counter').append('<p class="monthlyRate">$' + totalMonthly + ' per Month</p>');
+    //
+    // }
+    //
+    // $('.information').click(function() {
+    //     $(this).remove('.information');
+    // });
+
+
+
+}]);
